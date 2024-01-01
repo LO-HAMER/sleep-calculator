3514119ae3de3b622507750e704e83fa3203deed
LOHAMER <130869756+LO-HAMER@users.noreply.github.com>
2024-01-01 23:50:48 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,60 @@
 const getSleepHours = day => {
-    if (day === 'monday')
-    return 8;
-    };
-    
-    const getActualSleepHours = () => 
-      getSleepHours('monday')
-      +  getSleepHours('tuesday')
-      +  getSleepHours('wednesday')
-      +  getSleepHours('thursday')
-      +  getSleepHours('friday')
-      +  getSleepHours('saturday')
-      +  getSleepHours('sunday');
-      return getActualSleepHours
-    
-    
-    
-    console.log(getSleepHours('monday'));
+switch(day){
+  case 'monday':
+  return 8
+  break;
+    case 'tuesday':
+  return 7
+  break;
+    case 'wednesday':
+  return 7
+  break;
+    case 'thursday':
+  return 5
+  break;
+    case 'friday':
+  return 8
+  break;
+    case 'saturday':
+  return 7
+  break;
+    case 'sunday':
+  return 8
+  break;
+
+  default:
+  return 'Error'
+}
+};
+
+const getActualSleepHours = () => 
+  getSleepHours('monday')
+  +  getSleepHours('tuesday')
+  +  getSleepHours('wednesday')
+  +  getSleepHours('thursday')
+  +  getSleepHours('friday')
+  +  getSleepHours('saturday')
+  +  getSleepHours('sunday');
+
+const getIdealSleepHours = () =>{
+  const idealHours = 8;
+return idealHours * 7
+};
+
+const calculateSleepDebt = () => {
+const actualSleepHours = getActualSleepHours();
+const idealSleepHours = getIdealSleepHours();
+if (actualSleepHours === idealSleepHours){
+console.log("you've got the perfect amount of sleep");
+}
+if (actualSleepHours > idealSleepHours){
+console.log("you've got more sleep than needed");
+}
+if (actualSleepHours < idealSleepHours){
+console.log("you should get some rest");
+}
+};
+
+console.log(getActualSleepHours('monday'))
+console.log(getIdealSleepHours(7.5))
+console.log(getSleepHours('monday'));
